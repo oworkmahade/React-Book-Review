1cebae2e5206a628a15685e7e2b0fa256982d86c
mahade <owork.hasan@gmail.com>
2026-01-26 23:15:07 +0600

    commited

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,26 @@
 // import PropTypes from 'prop-types'
 
+import { NavLink } from "react-router-dom";
+
 function Header() {
+  const links = () => {
+    return (
+      <>
+        <li>
+          {" "}
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          {" "}
+          <NavLink to="/listed-books">Listed Books</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/pages-to-read">Pages To Read</NavLink>
+        </li>
+      </>
+    );
+  };
   return (
     <div>
       <div className="shadow-sm navbar bg-base-100">
@@ -27,31 +47,17 @@ function Header() {
               tabIndex="-1"
               className="p-2 mt-3 shadow menu menu-sm dropdown-content bg-base-100 rounded-box z-1 w-52"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-
-              <li>
-                <a>Item 3</a>
-              </li>
+              {links()}
             </ul>
           </div>
-          <a className="text-xl btn btn-ghost">daisyUI</a>
+          <a className="text-xl btn btn-ghost">Book Vibe</a>
         </div>
         <div className="hidden navbar-center lg:flex">
-          <ul className="px-1 menu menu-horizontal">
-            <li>
-              <a>Item 1</a>
-            </li>
-
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <ul className="px-1 menu menu-horizontal">{links()}</ul>
         </div>
         <div className="flex flex-row gap-4 navbar-end">
-          <a className="btn">Button</a>
-          <a className="btn">Button</a>
+          <a className="btn">Sign In</a>
+          <a className="btn">Sign Up</a>
         </div>
       </div>
     </div>
