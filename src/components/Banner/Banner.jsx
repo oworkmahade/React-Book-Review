229cfff173f568fb60f9944c5b6484e91c1f6c22
mahade <owork.hasan@gmail.com>
2026-01-30 22:43:08 +0600

    commited

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,19 @@
+import bannerImg from "../../assets/images/banner.png";
 const Banner = () => {
   return (
     <div>
       <div className="flex flex-col items-center justify-between gap-4 p-4 my-4 bg-gray-100 rounded-lg md:flex-row banner-container">
-        <div className="flex-1 border-2 border-red-600 banner-left">
-          <h1>Left</h1>
+        <div className="flex flex-col items-start justify-center flex-1 p-4 banner-left">
+          <h1 className="text-5xl font-bold">
+            Books to freshen up{" "}
+            <span className="leading-relaxed">your bookshelf</span>
+          </h1>
+          <button className="btn bg-[#23be0a] text-white mt-4">
+            View The List
+          </button>
         </div>
-        <div className="flex-1 border-2 border-blue-600 banner-right">
-          <h1>Right</h1>
+        <div className="flex items-center justify-center flex-1 p-4 banner-right">
+          <img className="h-96" src={bannerImg} alt="Banner" />
         </div>
       </div>
     </div>
