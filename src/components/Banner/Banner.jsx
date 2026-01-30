29c525f34ac4d9e693f07c24b111dde2d597e680
mahade <owork.hasan@gmail.com>
2026-01-30 22:06:21 +0600

    commited

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,14 @@
 const Banner = () => {
   return (
     <div>
-      <div className="p-4 my-4 bg-gray-100 rounded-lg"></div>
+      <div className="flex flex-col items-center justify-between gap-4 p-4 my-4 bg-gray-100 rounded-lg md:flex-row banner-container">
+        <div className="flex-1 border-2 border-red-600 banner-left">
+          <h1>Left</h1>
+        </div>
+        <div className="flex-1 border-2 border-blue-600 banner-right">
+          <h1>Right</h1>
+        </div>
+      </div>
     </div>
   );
 };
