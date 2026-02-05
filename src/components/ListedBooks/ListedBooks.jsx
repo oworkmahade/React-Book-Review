04e6c8f6b5e339eac152495e39c2f7b6155d1aa6
mahade <owork.hasan@gmail.com>
2026-02-06 00:03:49 +0600

    commited

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -3,8 +3,20 @@
 function ListedBooks() {
   return (
     <div>
-      <div className="py-12 my-8 text-center rounded-lg bg-slate-200">
+      <div className="py-12 my-8 text-center rounded-lg bg-[#f3f3f3]">
         <h2 className="text-2xl font-bold">Books</h2>
+      </div>
+
+      <div className="flex flex-row items-center justify-center ">
+        <select
+          className="font-semibold text-white bg-[#23be0a] rounded-lg select select-success"
+          defaultValue="Sort By"
+        >
+          <option disabled={true}>Sort By</option>
+          <option>Rating</option>
+          <option>Number of pages</option>
+          <option>Published year</option>
+        </select>
       </div>
     </div>
   );
